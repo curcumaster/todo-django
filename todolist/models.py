df4c0f58f2d69243c552a78eaa54dc96193f8a39
qq <qq@i.ua>
2022-08-25 16:12:29 +0300

    todolist

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,15 @@
+from re import M
+from tabnanny import verbose
 from django.db import models
 
-# Create your models here.
+
+class ToDo(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Задание')
+    is_complete = models.BooleanField(default=False, verbose_name='Готово')
+
+    class Meta():
+        verbose_name = 'Задание'
+        verbose_name_plural = 'Задания'
+
+    def __str__(self):
+        return self.title
